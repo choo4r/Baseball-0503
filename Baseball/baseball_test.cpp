0763928451bc0b34341bd31a08634e1b4c46751c
INHO <choo4r@gmail.com>
2025-07-02 13:56:49 +0900

    [faeture]1s2b

--- Baseball/baseball_test.cpp
+++ Baseball/baseball_test.cpp
@@ -36,7 +36,13 @@ TEST_F(BaseballFixture, ReturnTwoStrikesZeroBall) {
 	EXPECT_EQ(2, result.strikes);
 	EXPECT_EQ(0, result.balls);
 }
+TEST_F(BaseballFixture, ReturnOneStrikesTwoBall) {
+	GuessResult result = game.guess("132");
 
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(1, result.strikes);
+	EXPECT_EQ(2, result.balls);
+}
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
